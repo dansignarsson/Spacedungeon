8decc5beca7af20560591714b0f10788aa66d4b6
JonasRing <jonas.hg.ring@gmail.com>
2019-03-08 16:12:20 +0100

    Get the power of the shield!

---
+++ DungeonsOfDoomStart/DungeonsOfDoom/Shield.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsOfDoom
+{
+    class Shield: DefenseItem
+    {
+        public Shield(string name, int defensePoints) : base(name, defensePoints)
+        {
+        }
+
+        public override void PickUp(Character characterToPick)
+        {
+            characterToPick.Health += 100; 
+        }
+    }
+}
