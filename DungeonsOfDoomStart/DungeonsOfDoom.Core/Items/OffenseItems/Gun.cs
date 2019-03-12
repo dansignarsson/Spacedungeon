4d3d8f7340f152646bb4a05260eb89ab3f75fde7
em85ma <emily@magnusson.nu>
2019-03-12 16:21:12 +0100

    New gunfile

---
+++ DungeonsOfDoomStart/DungeonsOfDoom.Core/Items/OffenseItems/Gun.cs
@@ -0,0 +1,23 @@
+﻿using DungeonsOfDoom.Core.Characters;
+    using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsOfDoom.Core.Items.OffenseItems
+{
+    class Gun : OffenseItem
+    {
+        public Gun(string name, int attackPoints) : base(name, attackPoints)
+        {
+
+
+        }
+
+        public override void PickUp(Character monsterAttack)
+        {
+
+
+            monsterAttack.Health -= 15;
+        }
+    }
+}
